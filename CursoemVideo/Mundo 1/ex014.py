991d4e17e0977258552bf267516ce5a076db9008
Fábio Moreira Silva <fabio.silva174@fatec.sp.gov.br>
2021-07-22 11:31:37 -0300

    Delete CursoemVideo/Mundo 1 directory

--- CursoemVideo/Mundo 1/ex014.py
+++
@@ -1,5 +0,0 @@
-print('==== Desafio 14 ====')
-#Conversor de graus Celsius para Fahrenheit
-celc = float(input('Qual a temperatura °C: '))
-fahre = (9 * celc + 160) / 5
-print('A temperatura de {}°C corresponde a {}°F.'.format(celc, fahre))
